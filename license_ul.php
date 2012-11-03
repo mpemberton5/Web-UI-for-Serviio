43c3a273b0ca0ab249a58e109b93e17ef68731d7
Mark Pemberton <mpemberton5@gmail.com>
2012-11-03 02:35:45 -0400

    adding new files

---
+++ license_ul.php
@@ -0,0 +1,12 @@
+<?php
+include("config.php");
+include("lib/RestRequest.inc.php");
+include("lib/serviio.php");
+
+if (substr($_FILES['uploadFile']['name'],-4) == ".lic") {
+    $serviio = new ServiioService($serviio_host,$serviio_port);
+    $res = $serviio->PutLicenseUpload(file_get_contents($_FILES['uploadFile']['tmp_name']));
+} else {
+    echo "<result>\n  <errorCode>99999</errorCode>\n  <Text>Error! File doesn't appear to be license file.</Text>\n</result>";
+}
+?>

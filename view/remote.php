8c06b25fc8520300072a481d0c8a1bb983014576
SwoopX <rol_swoop@gmx.de>
2013-04-02 16:12:44 +0300

    Update remote.php
    - Unified button layout
    - Added enable port mapping checkbox, external address and check port mapping button
    - Variable $quality now gets data from getReferenceData

--- view/remote.php
+++ view/remote.php
@@ -1,7 +1,8 @@
 <form method="post" action="" id="remoteform">
 <input type="hidden" name="tab" value="remote">
+<input type="hidden" id="process" name="process" value="">
 <br />
-<?php echo tr('tab_remote_choose_settings','Choose your settings for remote access to your files.')?>
+<?php echo tr('tab_remote_choose_settings','Choose your settings for remote access to your files. If you want to access the server over the Internet, you have to set up your router for incoming outside connections.')?>
 <br />
 <ul id="rmtSecurityTab" class="shadetabs">
     <li><a href="#" rel="rmt1" class="selected"><?php echo tr('tab_remote_security','Security')?></a></li>
@@ -25,11 +26,33 @@
         <br>
         <?php echo tr('tab_remote_preferred_delivery_quality','Preferred delivery quality:')?>
         <select id="deliveryQuality" name="deliveryQuality">
-            <option value="LOW"<?php echo $rmtAccess['preferredRemoteDeliveryQuality']=="LOW"?" selected":""?>>Low</option>
-            <option value="MEDIUM"<?php echo $rmtAccess['preferredRemoteDeliveryQuality']=="MEDIUM"?" selected":""?>>Medium</option>
-            <option value="ORIGINAL"<?php echo $rmtAccess['preferredRemoteDeliveryQuality']=="ORIGINAL"?" selected":""?>>Original</option>
+            
+        	<?php foreach ($quality as $key=>$val) { ?>
+			<option value="<?php echo $key?>"<?php echo $key==$rmtAccess['preferredRemoteDeliveryQuality']?" selected":""?>><?php echo $val?></option>
+			<?php } ?>
+        	
         </select>
         <br>
+    </div>
+</div>
+
+<br>
+
+<ul id="rmtInternetAccessTab" class="shadetabs">
+    <li><a href="#" rel="rmt3" class="selected"><?php echo tr('tab_remote_internet_access','Internet access')?></a></li>
+</ul>
+<div style="border:1px solid gray; width:98%; margin-bottom: 1em; padding: 10px">
+    <div id="rmt3" class="tabcontent">
+        <br>
+        <input type="checkbox" name="portMapping" value="1"<?php echo $rmtAccess['portMappingEnabled']=="true"?" checked":""?>> <?php echo tr('tab_remote_port_mapping','Automatically configure your router to allow incoming internet connections')?>
+        <br>
+        <br>
+        <?php echo tr('tab_remote_external_address','External address:')?>
+        <input type="text" id="extAddress" name="extAddress" size="64" value="<?php echo $rmtAccess['externalAddress']?>">
+        <br>
+        <br>
+        <input type="submit" id="checkPortMapping" name="checkPortMapping" value="<?php echo tr('tab_remote_internet_access_button_check_port_mapping','Check connectivity status')?>" class="ui-button ui-widget ui-state-default ui-corner-all btn-small" />
+		<span id="checkPortMappingMsg" class="checkPortMappingMsg"></span>
     </div>
 </div>
 
@@ -38,8 +61,8 @@
 </div>
 <div align="right" style="float: right;">
     <span id="savingMsg" class="savingMsg"></span>
-    <input type="submit" id="reset" name="reset" value="<?php echo tr('button_reset','Reset')?>" onclick="return confirm('Are you sure you want to reset changes?')">
-    <input type="submit" id="submit" name="save" value="<?php echo tr('button_save','Save')?>" />
+    <input type="submit" id="reset" name="reset" value="<?php echo tr('button_reset','Reset')?>" onclick="return confirm('Are you sure you want to reset changes?')" class="ui-button ui-widget ui-state-default ui-corner-all btn-small" />
+    <input type="submit" id="submit" name="save" value="<?php echo tr('button_save','Save')?>" class="ui-button ui-widget ui-state-default ui-corner-all btn-small" />
 </div>
 <br />
 </form>

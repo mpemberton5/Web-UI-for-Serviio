fb2239083f3a1c0d40a0a3b4214f8e287fc7f95f
SwoopX <rol_swoop@gmx.de>
2013-04-02 18:40:52 +0300

    Added subtitle variables

--- code/transcoding1.php
+++ code/transcoding1.php
@@ -12,7 +12,14 @@ $location = getPostVar("location","");
 $cores = getPostVar("cores","1");
 $audio = getPostVar("audio","")=="downmix"?"true":"false";
 $quality = getPostVar("quality","0")==1?"true":"false";
-$errorCode = $serviio->putTranscoding($transcoding,$location,$cores,$audio,$quality);
+
+$subtitles = getPostVar("subtitles","0")==1?"true":"false";
+$subtitlesextraction = getPostVar("subtitlesextraction","0")==1?"true":"false";
+$hardsubsenabled = getPostVar("hardsubsenabled","0")==1&&(getPostVar("hardsubs","")=="enabled"||getPostVar("hardsubs","")=="")?"true":"false";
+$hardsubsforced = getPostVar("hardsubsenabled","0")==1&&getPostVar("hardsubs","0")=="forced"?"true":"false";
+$language = getPostVar("language","");
+
+$errorCode = $serviio->putDelivery($transcoding,$location,$cores,$audio,$quality,$subtitles,$subtitlesextraction,$hardsubsenabled,$hardsubsforced,$language);
 if ($errorCode===false || $errorCode!=0) {
     $message = $serviio->warning;
 }

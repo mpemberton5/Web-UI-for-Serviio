26db00995c6478aeaf74be98d2339be36538436d
SwoopX <rol_swoop@gmx.de>
2013-04-02 18:32:57 +0300

    Update status2.php
    - Changed varialbe $ip to $bound_nic
    - Added variable $accesses to fetch accessGroups from getReferenceData
    - Added variable $profiles to fetch profiles from getReferenceData
    - Added variable $interfaces to fetch networkInterfaces from getReferenceData

--- code/status2.php
+++ code/status2.php
@@ -38,8 +38,8 @@ if ($_SERVER["REQUEST_METHOD"]=="POST") {
                 }
             }
         }
-        $ip = getPostVar("ip", "");
-        $errorCode = $serviio->putStatus($profiles, $ip);
+        $bound_nic = getPostVar("bound_nic", "");
+        $errorCode = $serviio->putStatus($profiles, $bound_nic);
     } else if (getPostVar("start", "")!="") {
         $errorCode = $serviio->postAction("startServer");
     } else if (getPostVar("stop", "")!="") {
@@ -55,7 +55,9 @@ if ($statusResponse["serverStatus"] == "STARTED") {
     $startDisabled = "disabled";
     $stopDisabled = "";
 }
-$profiles = $serviio->getProfiles();
+$profiles = $serviio->getReferenceData('profiles');
+$interfaces = $serviio->getReferenceData('networkInterfaces');
+$accesses = $serviio->getReferenceData('accessGroups');
 
 function status_icon($status)
 {

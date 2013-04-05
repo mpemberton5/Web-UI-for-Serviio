9b1a2c08b97e3bd441f476e18a9f87f0eb3deb05
SwoopX <rol_swoop@gmx.de>
2013-04-05 14:52:02 +0300

    Improved multilanguage support

--- view/remote.php
+++ view/remote.php
@@ -3,14 +3,15 @@
 <input type="hidden" id="process" name="process" value="">
 <br />
 <?php echo tr('tab_remote_choose_settings','Choose your settings for remote access to your files. If you want to access the server over the Internet, you have to set up your router for incoming outside connections.')?>
-<br />
+<br>
+<br>
 <ul id="rmtSecurityTab" class="shadetabs">
     <li><a href="#" rel="rmt1" class="selected"><?php echo tr('tab_remote_security','Security')?></a></li>
 </ul>
 <div style="border:1px solid gray; width:98%; margin-bottom: 1em; padding: 10px">
     <div id="rmt1" class="tabcontent">
         <br>
-        <?php echo tr('tab_remote_user_password','User Password:')?>
+        <?php echo tr('tab_remote_user_password','User Password')?>:&nbsp;
         <input type="password" id="userPasswd" name="userPasswd" size="40" value="<?php echo $rmtAccess['remoteUserPassword']?>">
         <br>
     </div>
@@ -24,9 +25,9 @@
 <div style="border:1px solid gray; width:98%; margin-bottom: 1em; padding: 10px">
     <div id="rmt2" class="tabcontent">
         <br>
-        <?php echo tr('tab_remote_preferred_delivery_quality','Preferred delivery quality:')?>
+        <?php echo tr('tab_remote_preferred_delivery_quality','Preferred delivery quality')?>:&nbsp;
         <select id="deliveryQuality" name="deliveryQuality">
-            
+        	
         	<?php foreach ($quality as $key=>$val) { ?>
 			<option value="<?php echo $key?>"<?php echo $key==$rmtAccess['preferredRemoteDeliveryQuality']?" selected":""?>><?php echo $val?></option>
 			<?php } ?>
@@ -47,23 +48,23 @@
         <input type="checkbox" name="portMapping" value="1"<?php echo $rmtAccess['portMappingEnabled']=="true"?" checked":""?>> <?php echo tr('tab_remote_port_mapping','Automatically configure your router to allow incoming internet connections')?>
         <br>
         <br>
-        <?php echo tr('tab_remote_external_address','External address:')?>
+        <?php echo tr('tab_remote_external_address','External address')?>:&nbsp;
         <input type="text" id="extAddress" name="extAddress" size="64" value="<?php echo $rmtAccess['externalAddress']?>">
         <br>
         <br>
-        <input type="submit" id="checkPortMapping" name="checkPortMapping" value="<?php echo tr('tab_remote_internet_access_button_check_port_mapping','Check connectivity status')?>" class="ui-button ui-widget ui-state-default ui-corner-all btn-small" />
+        <input type="submit" id="checkPortMapping" name="checkPortMapping" value="<?php echo tr('button_check_port_mapping','Check connectivity status')?>" class="ui-button ui-widget ui-state-default ui-corner-all btn-small" />
 		<span id="checkPortMappingMsg" class="checkPortMappingMsg"></span>
     </div>
 </div>
 
 <div style="float: left;">
-    <?php foreach ($interfaces as $key=>$val) { ?>
-    <?php if($key==$boundNIC['boundNICName']) echo '<a href="http://'.substr($val, 0, -7).':23424/mediabrowser/" target="_blank">Open MediaBrowser</a>'; ?>
-    <?php }?>
+	<?php foreach ($interfaces as $key=>$val) { ?>
+	<?php if($key==$boundNIC['boundNICName']) echo '<a href="http://'.substr($val, 0, -7).':23424/mediabrowser/" target="_blank">Open MediaBrowser</a>'; ?>
+	<?php }?>
 </div>
 <div align="right" style="float: right;">
     <span id="savingMsg" class="savingMsg"></span>
-    <input type="submit" id="reset" name="reset" value="<?php echo tr('button_reset','Reset')?>" onclick="return confirm('Are you sure you want to reset changes?')" class="ui-button ui-widget ui-state-default ui-corner-all btn-small" />
+    <input type="submit" id="reset" name="reset" value="<?php echo tr('button_reset','Reset')?>" onclick="return confirm('<?php echo tr('status_message_reset','Are you sure you want to reset changes?')?>')" class="ui-button ui-widget ui-state-default ui-corner-all btn-small" />
     <input type="submit" id="submit" name="save" value="<?php echo tr('button_save','Save')?>" class="ui-button ui-widget ui-state-default ui-corner-all btn-small" />
 </div>
 <br />

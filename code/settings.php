35829866a6211fe4c7fadb51708188b3a3ef537e
SwoopX <rol_swoop@gmx.de>
2013-04-05 15:59:06 +0300

    Added cookie path
    Now, the language variable in cookie is taken into account for displaying the selected language.

--- code/settings.php
+++ code/settings.php
@@ -2,7 +2,7 @@
 if (isset($_POST["language"]) && array_key_exists($_POST["language"],$languages)) {
     $language = $_POST["language"];
     $_COOKIE["language"] = $language;
-    setcookie("language",$language,mktime(9,9,9,9,9,9999));
+    setcookie("language",$language,mktime(9,9,9,9,9,9999),"/");
 }
 $settings = $serviio->getConsoleSettings();
 ?>

9831583c44194de3d56c915954397b08f090e000
SwoopX <rol_swoop@gmx.de>
2013-05-31 23:51:25 +0200

    Updated config.php - Added variable for Serviio log file

--- config.php
+++ config.php
@@ -8,6 +8,7 @@ $serviio_host = "127.0.0.1";
 $serviio_port = "23423";
 $serviidb_url = "http://www.serviidb.com/api/";
 $version_req = "1.0.1";
+$serviio_log = "";
 
 # set appropriate encoding
 mb_internal_encoding("UTF-8");

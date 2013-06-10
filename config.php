a106fe5219bc9f3caba03836dbefb94afa5e7b11
SwoopX <rol_swoop@gmx.de>
2013-06-10 12:26:37 +0200

    Update config.php
    - Removed obsolete variables $language and $serviio_mediabrowser_host.
    - Updated required version.
    - Removed languages array as language is now determined via built-in functionality.

--- config.php
+++ config.php
@@ -1,45 +1,18 @@
 <?php
-$language = "en";
-$webUIver = "1.4beta2";
-$debugLevel = "none"; // none, debug
-$debugLoc = "none"; // none, screen
-$serviio_mediabrowser_host = "127.0.0.1";
-$serviio_host = "127.0.0.1";
-$serviio_port = "23423";
-$serviidb_url = "http://www.serviidb.com/api/";
-$version_req = "1.0.1";
-$serviio_log = "";
+    $webUIver = "1.4beta3";
+	$debugLevel = "none"; // none, debug
+	$debugLoc = "none"; // none, screen
+	$serviio_host = "127.0.0.1";
+	$serviio_port = "23423";
+	$serviidb_url = "http://www.serviidb.com/api/";
+	$version_req = "1.2.1";
+	$serviio_log = "";
 
-# set appropriate encoding
-mb_internal_encoding("UTF-8");
+	# set appropriate encoding
+	mb_internal_encoding("UTF-8");
 
-if ($debugLevel == "debug") {
-    ini_set('display_errors', 1);
-    ini_set('error_reporting', E_ALL);
-}
-
-# Available console languages (bundled)
-$languages = array(
-'bg' => 'Български',
-'ca' => 'Català',
-'cs' => 'Čeština',
-'da' => 'Dansk',
-'de' => 'Deutsch',
-'en' => 'English',
-'es' => 'Español',
-'fr' => 'Français',
-'hu' => 'Magyar',
-'it' => 'Italiano',
-'nl' => 'Nederlands',
-'no' => 'Norsk',
-'pl' => 'Polski',
-'pt_BR' => 'Português (Brasil)',
-'ro' => 'Română',
-'ru' => 'Русский',
-'sk' => 'Slovenščina',
-'sv' => 'Svenska',
-'zh_CN' => '中文 (Simplified)',
-'zh_HK' => '中文 (Traditional)'
-);
-
+	if ($debugLevel == "debug") {
+		ini_set('display_errors', 1);
+		ini_set('error_reporting', E_ALL);
+	}
 ?>

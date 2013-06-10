9cb6a9bd269863e89af7e4f1951e117e620fe6b7
SwoopX <rol_swoop@gmx.de>
2013-06-10 13:43:31 +0300

    delivery1.php - File is obsolete
    Functionality has been implemented in delivery.php

--- code/delivery1.php
+++ code/delivery1.php
@@ -1,27 +1,3 @@
 <?php
-include("../config.php");
-include("../lib/RestRequest.inc.php");
-include("../lib/serviio.php");
 
-// initiate call to service
-$serviio = new ServiioService($serviio_host,$serviio_port);
-
-$errorCode = 0;
-$transcoding = getPostVar("transcoding","0")==1?"true":"false";
-$location = getPostVar("location","");
-$cores = getPostVar("cores","1");
-$audio = getPostVar("audio","")=="downmix"?"true":"false";
-$quality = getPostVar("quality","0")==1?"true":"false";
-
-$subtitles = getPostVar("subtitles","0")==1?"true":"false";
-$subtitlesextraction = getPostVar("subtitlesextraction","0")==1?"true":"false";
-$hardsubsenabled = getPostVar("hardsubsenabled","0")==1&&(getPostVar("hardsubs","")=="enabled"||getPostVar("hardsubs","")=="")?"true":"false";
-$hardsubsforced = getPostVar("hardsubsenabled","0")==1&&getPostVar("hardsubs","0")=="forced"?"true":"false";
-$language = getPostVar("language","");
-
-$errorCode = $serviio->putDelivery($transcoding,$location,$cores,$audio,$quality,$subtitles,$subtitlesextraction,$hardsubsenabled,$hardsubsforced,$language);
-if ($errorCode===false || $errorCode!=0) {
-    $message = $serviio->warning;
-}
-return $errorCode;
 ?>

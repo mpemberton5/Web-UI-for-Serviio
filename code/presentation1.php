736d9522102cedae9d40484dc03d8e8c82e4d77b
SwoopX <rol_swoop@gmx.de>
2013-06-10 13:45:33 +0300

    presentation1.php - File is obsolete
    Functionality has been implemented in presentation.php

--- code/presentation1.php
+++ code/presentation1.php
@@ -1,36 +1,3 @@
 <?php
-include("../config.php");
-include("../lib/RestRequest.inc.php");
-include("../lib/serviio.php");
 
-// initiate call to service
-$serviio = new ServiioService($serviio_host,$serviio_port);
-
-$errorCode = 0;
-$categories = array();
-$presentation_language = getPostVar("presentation_language", "en");
-$showParentCategoryTitle = getPostvar("showParentCategoryTitle", "0")=="1"?"true":"false";
-$numberOfFilesForDynamicCategories = getPostvar("numberOfFilesForDynamicCategories", "10");
-
-$titles = isset($_POST['titles'])?$_POST['titles']:0;
-$visibilities = isset($_POST['visibility'])?$_POST['visibility']:0;
-
-if (is_array($titles) && count($titles)>0 && is_array($visibilities) && count($visibilities)>0) {
-    foreach ($titles as $id=>$entry) {
-        $title = "";
-        $visibility = "";
-        $subCategories = array();
-        foreach ($entry as $sid=>$val) {
-            if ($sid!="title") {
-                $subVisibility = $visibilities[$id][$sid];
-                $subCategories[$sid] = array($val, $subVisibility);
-            }
-        }
-        $title = $titles[$id][title];
-        $visibility = $visibilities[$id][visibility];
-        $categories[$id] = array($title, $visibility, $subCategories);
-    }
-}
-$errorCode = $serviio->putPresentation($categories, $presentation_language, $showParentCategoryTitle, $numberOfFilesForDynamicCategories);
-return $errorCode;
 ?>

d1181e19db8f3022eb30f3a13dfd9a8658b2f113
SwoopX <rol_swoop@gmx.de>
2013-06-10 13:51:53 +0300

    Merged functionality of all settingsX.php files
    - Additional code cleanup.
    - Language is now determined via Serviio built-in functionality.

--- code/settings.php
+++ code/settings.php
@@ -1,8 +1,25 @@
 <?php
-if (isset($_POST["language"]) && array_key_exists($_POST["language"],$languages)) {
-    $language = $_POST["language"];
-    $_COOKIE["language"] = $language;
-    setcookie("language",$language,mktime(9,9,9,9,9,9999),"/");
-}
-$settings = $serviio->getConsoleSettings();
+    if ($_SERVER["REQUEST_METHOD"]=="POST") {
+		include("../config.php");
+		include("../lib/RestRequest.inc.php");
+		include("../lib/serviio.php");
+
+		// initiate call to service
+		$serviio = new ServiioService($serviio_host,$serviio_port);
+
+		/*****************************************************************/
+		/*****************************************************************/
+		if (getPostVar("process", "") == "save") {
+			$errorCode = 0;
+			
+			$retLang = getPostVar("language","en");
+			$retCheckForUpdates = getPostVar("checkForUpdates","0")==1?"true":"false";
+			$errorCode = $serviio->putConsoleSettings($retLang,$retCheckForUpdates);
+
+			return $errorCode;
+		}
+	}
+	
+	$settings = $serviio->getConsoleSettings();
+	$languages = $serviio->getReferenceData('browsingCategoriesLanguages');
 ?>

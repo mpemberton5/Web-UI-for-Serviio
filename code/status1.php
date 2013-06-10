2d6a829dbb4c6302cff85cc8dbe62744b8e18537
SwoopX <rol_swoop@gmx.de>
2013-06-10 13:46:58 +0300

    status1.php - File is obsolete
    Functionality has been implemented in status.php

--- code/status1.php
+++ code/status1.php
@@ -1,31 +1,3 @@
 <?php
-include("../config.php");
-include("../lib/RestRequest.inc.php");
-include("../lib/serviio.php");
 
-// initiate call to service
-$serviio = new ServiioService($serviio_host,$serviio_port);
-
-$errorCode = 0;
-
-$zero = $_POST;
-$profiles = array();
-foreach ($_POST as $key=>$val) {
-    if (substr($key, 0, 5)=="name_") {
-        $uuid = substr($key, 5);
-        if ($uuid!="") {
-            $ipAddress = getPostVar("ipAddress_${uuid}", "");
-            $name = $val;
-            $profile = getPostVar("profile_${uuid}", "1"); // Generic DLNA profile
-            $enabled = getPostVar("enabled_${uuid}", "");
-            $accGrpId = getPostVar("access_${uuid}", "1");
-            $profiles[] = array($uuid, $ipAddress, $name, $profile, $enabled, $accGrpId);
-        }
-    }
-}
-$bound_nic = getPostVar("bound_nic", "");
-$errorCode = $serviio->putStatus($profiles, $bound_nic);
-return $errorCode;
-return print_r($zero);
-return print_r($zero["tab"]);
 ?>

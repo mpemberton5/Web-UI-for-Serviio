ebd4192daa181f990df565d257956d39acc9c204
SwoopX <rol_swoop@gmx.de>
2013-06-10 13:29:16 +0300

    Content.php - Language is now determined via built-in functionality.

--- content.php
+++ content.php
@@ -1,19 +1,19 @@
 <?php
-set_time_limit(0);
-include("config.php");
-include("lib/RestRequest.inc.php");
-include("lib/serviio.php");
+	set_time_limit(0);
+	include("config.php");
+	include("lib/RestRequest.inc.php");
+	include("lib/serviio.php");
 
-$tab = isset($_REQUEST["tab"])?$_REQUEST["tab"]:"";
-if ($tab!="library" && $tab!="metadata" && $tab!="delivery" && $tab!="about" && $tab!="presentation" && $tab!="remote" && $tab!="settings" && $tab!="logs" && $tab!="about") {
-    $tab = "status";
-}
-if (isset($_COOKIE["language"]) && array_key_exists($_COOKIE["language"],$languages)) {
-    $language = $_COOKIE["language"];
-}
+	$tab = isset($_REQUEST["tab"])?$_REQUEST["tab"]:"";
+	if ($tab!="library" && $tab!="metadata" && $tab!="delivery" && $tab!="about" && $tab!="presentation" && $tab!="remote" && $tab!="settings" && $tab!="logs" && $tab!="about") {
+		$tab = "status";
+	}
 
-$serviio = new ServiioService($serviio_host,$serviio_port);
+	$serviio = new ServiioService($serviio_host,$serviio_port);
 
-include("code/${tab}.php");
-include("view/${tab}.php");
+	$settings = $serviio->getConsoleSettings();
+	$language = $settings["language"];
+
+	include("code/${tab}.php");
+	include("view/${tab}.php");
 ?>
